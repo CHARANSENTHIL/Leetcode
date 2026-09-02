77d099ffd046b10db6ff16e0ef01fd83108a3e6f
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-02 12:08:49 +0530

    Time: 95 ms (82.03%), Space: 32.5 MB (46.87%) - LeetHub

---
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeAdjacent(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            while stack and stack[-1] == num:
+                num += stack.pop()
+            stack.append(num)
+
+        return stack

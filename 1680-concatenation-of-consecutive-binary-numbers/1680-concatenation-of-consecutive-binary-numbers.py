7e9180ea2fc0772147c6af0c3eb4f6db078c94d8
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-03 20:39:04 +0530

    Time: 668 ms (68.15%), Space: 19.1 MB (71.53%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def concatenatedBinary(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        result = l = 0
+        for i in range(1, n+1):
+            if i&(i-1) == 0:
+                l += 1
+            result = ((result<<l)%MOD+i)%MOD
+        return result

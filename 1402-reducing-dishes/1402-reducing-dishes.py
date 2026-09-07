02d405b685ef6454825cfc441c1182f74fd3e04f
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-07 20:11:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (42.71%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSatisfaction(self, satisfaction):
+        """
+        :type satisfaction: List[int]
+        :rtype: int
+        """
+        satisfaction.sort(reverse=True)
+        result, curr = 0, 0
+        for x in satisfaction:
+            curr += x
+            if curr <= 0:
+                break
+            result += curr
+        return result

d322191eb0808be6591f6045bb64f490b90aff8f
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-07 20:12:30 +0530

    Time: 34 ms (91.54%), Space: 20.2 MB (93.53%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
@@ -1,3 +1,50 @@
+import collections
+import string
+
+
 class Solution:
-    def checkIfCanBreak(self, s: str, t: str) -> bool:
-        return all(map(le,*sorted(map(sorted,(s,t)))))
+    def checkIfCanBreak(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        def is_break(count1, count2):
+            curr1, curr2 = 0, 0
+            for c in string.ascii_lowercase:
+                curr1 += count1[c]
+                curr2 += count2[c]
+                if curr1 < curr2:
+                    return False
+            return True
+
+        count1, count2 = collections.Counter(s1), collections.Counter(s2)
+        return is_break(count1, count2) or is_break(count2, count1)
+    
+
+import itertools
+
+
+class Solution2(object):
+    def checkIfCanBreak(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return not {1, -1}.issubset(set(cmp(a, b) for a, b in zip(sorted(s1), sorted(s2))))
+
+
+import itertools
+
+
+class Solution3(object):
+    def checkIfCanBreak(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        s1, s2 = sorted(s1), sorted(s2)
+        return all(a >= b for a, b in zip(s1, s2)) or \
+               all(a <= b for a, b in zip(s1, s2))

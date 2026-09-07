377395f79feb6595de8e5c57fad1677925230945
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-07 19:54:36 +0530

    Time: 6 ms (38.47%), Space: 19.4 MB (32.1%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortByBits(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        def popcount(n):  # Time: O(logn) ~= O(1) if n is a 32-bit number
+            result = 0
+            while n:
+                n &= n - 1
+                result += 1
+            return result
+        
+        arr.sort(key=lambda x: (popcount(x), x))
+        return arr

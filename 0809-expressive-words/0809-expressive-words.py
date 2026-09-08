ed33ba002432bbbc1707e41642656ca14bc714f1
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:18:48 +0530

    Time: 11 ms (32.27%), Space: 19.4 MB (22.74%) - LeetHub

---
+++ 0809-expressive-words/0809-expressive-words.py
@@ -0,0 +1,24 @@
+import itertools
+
+
+class Solution:
+    def expressiveWords(self, S, words):
+        """
+        :type S: str
+        :type words: List[str]
+        :rtype: int
+        """
+        # Run length encoding
+        def RLE(S):
+            return zip(*[(k, len(list(grp)))
+                                  for k, grp in itertools.groupby(S)])
+
+        R, count = RLE(S)
+        result = 0
+        for word in words:
+            R2, count2 = RLE(word)
+            if R2 != R:
+                continue
+            result += all(c1 >= max(c2, 3) or c1 == c2
+                          for c1, c2 in zip(count, count2))
+        return result

053605773ef1ad2e49065e1caaf6fea8e36c7044
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:34:05 +0530

    Time: 191 ms (6.49%), Space: 42.7 MB (50.65%) - LeetHub

---
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.py
@@ -0,0 +1,18 @@
+import collections
+
+
+# freq table, combinatorics
+class Solution:
+    def beautifulSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        cnt = collections.Counter()
+        cnt[0] = 1
+        result = curr = 0
+        for x in nums:
+            curr ^= x
+            result += cnt[curr]
+            cnt[curr] += 1
+        return result

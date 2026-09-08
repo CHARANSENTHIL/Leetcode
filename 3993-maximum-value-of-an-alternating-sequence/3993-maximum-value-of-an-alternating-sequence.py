5e394c45a70d3295d23d80f29b6aa4a70fff8753
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:53:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (21.28%) - LeetHub

---
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.py
@@ -0,0 +1,10 @@
+# math
+class Solution:
+    def maximumValue(self, n, s, m):
+        """
+        :type n: int
+        :type s: int
+        :type m: int
+        :rtype: int
+        """
+        return s+(n//2)*m-((n-1)//2)*1+(n%2 if n != 1 else 0)

db1a95320a57cac7f6ef5f3ebdb7a1491bde0257
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:31:59 +0530

    Time: 80 ms (42.08%), Space: 21.9 MB (86.34%) - LeetHub

---
+++ 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.py
@@ -0,0 +1,14 @@
+# prefix sum
+class Solution:
+    def divisibilityArray(self, word, m):
+        """
+        :type word: str
+        :type m: int
+        :rtype: List[int]
+        """
+        result = []
+        curr = 0
+        for c in word:
+            curr = (curr*10+(ord(c)-ord('0')))%m
+            result.append(int(curr == 0))
+        return result

7b2c9523b61613fe815e6d17b36a535c2fa79b4f
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 13:11:24 +0530

    Time: 310 ms (31.22%), Space: 19.3 MB (70%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -0,0 +1,15 @@
+import collections
+import itertools
+
+
+class Solution:
+    def countBalls(self, lowLimit, highLimit):
+        """
+        :type lowLimit: int
+        :type highLimit: int
+        :rtype: int
+        """
+        count = collections.Counter()
+        for i in range(lowLimit, highLimit+1):
+            count[sum(map(int, str(i)))] += 1
+        return max(count.values())

d2fa1044e5870198cec13887c3e491fdf4da5017
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 13:08:12 +0530

    Time: 615 ms (55.71%), Space: 49.9 MB (75.24%) - LeetHub

--- 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.py
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.py
@@ -1,12 +1,49 @@
+import random
+
+
 class Solution:
-    def kthLargestValue(self, matrix: List[List[int]], k: int) -> int:
-        m, n = len(matrix), len(matrix[0]) # dimensions 
+    def kthLargestValue(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def nth_element(nums, n, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+
+            left, right = 0, len(nums)-1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
         
-        ans = []
-        for i in range(m): 
-            for j in range(n): 
-                if i: matrix[i][j] ^= matrix[i-1][j]
-                if j: matrix[i][j] ^= matrix[i][j-1]
-                if i and j: matrix[i][j] ^= matrix[i-1][j-1]
-                ans.append(matrix[i][j])
-        return sorted(ans)[-k] 
+        
+        vals = []
+        for r in range(len(matrix)):
+            curr = 0
+            for c in range(len(matrix[0])):
+                curr = curr^matrix[r][c]
+                if r == 0:
+                    matrix[r][c] = curr
+                else:
+                    matrix[r][c] = curr^matrix[r-1][c]
+                vals.append(matrix[r][c])
+        nth_element(vals, k-1, compare=lambda a, b: a > b)
+        return vals[k-1]

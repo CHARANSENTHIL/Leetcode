996da1ef7f599f714d31410ed7e0c0c852472014
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:18:28 +0530

    Time: 3 ms (79.71%), Space: 19.3 MB (36.85%) - LeetHub

---
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -0,0 +1,15 @@
+import itertools
+
+
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        row_maxes = [max(row) for row in grid]
+        col_maxes = [max(col) for col in zip(*grid)]
+
+        return sum(min(row_maxes[r], col_maxes[c])-val \
+                   for r, row in enumerate(grid) \
+                   for c, val in enumerate(row))

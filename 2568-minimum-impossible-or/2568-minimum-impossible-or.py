f66c7c1890ccfa37d402dc042d7153e5205fce03
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:30:40 +0530

    Time: 20 ms (29.13%), Space: 36.6 MB (37.86%) - LeetHub

---
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.py
@@ -0,0 +1,9 @@
+# hash table, bit manipulations
+class Solution:
+    def minImpossibleOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lookup = set(nums)
+        return next(1<<i for i in range(31) if 1<<i not in lookup)

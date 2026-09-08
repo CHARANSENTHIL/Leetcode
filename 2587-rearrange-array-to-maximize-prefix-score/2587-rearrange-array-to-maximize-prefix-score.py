ff8ae98b56aa4df83c79ea727d248e1d5354fef0
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:33:49 +0530

    Time: 83 ms (91.77%), Space: 32 MB (36.36%) - LeetHub

---
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.py
@@ -0,0 +1,14 @@
+# sort, greedy
+class Solution:
+    def maxScore(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        curr = 0
+        for i, x in enumerate(nums):
+            curr += x
+            if curr <= 0:
+                return i
+        return len(nums)

3a717e4232d3715359d684ec86e3c3512c74ef0b
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:19:28 +0530

    Time: 62 ms (11.47%), Space: 19.3 MB (93.18%) - LeetHub

--- 0812-largest-triangle-area/0812-largest-triangle-area.py
+++ 0812-largest-triangle-area/0812-largest-triangle-area.py
@@ -1,18 +1,18 @@
-class Solution(object):
+class Solution:
     def largestTriangleArea(self, points):
-        length = len(points)
-        area = 0
-        for i in range(length):
-            for j in range(i, length):
-                for k in range (j, length):
-                    x1 = points[i][0]
-                    x2 = points[j][0]
-                    x3 = points[k][0]
-
-                    y1 = points[i][1]
-                    y2 = points[j][1]
-                    y3 = points[k][1]
-
-                    area = max(abs(0.5 * (x1*(y2-y3) + x2*(y3 - y1) + x3*(y1-y2))), area)
-        return area
-      
+        """
+        :type points: List[List[int]]
+        :rtype: float
+        """
+        result = 0
+        for i in range(len(points)-2):
+            for j in range(i+1, len(points)-1):
+                for k in range(j+1, len(points)):
+                    result = max(result,
+                                 0.5 * abs(points[i][0] * points[j][1] +
+                                           points[j][0] * points[k][1] +
+                                           points[k][0] * points[i][1] -
+                                           points[j][0] * points[i][1] -
+                                           points[k][0] * points[j][1] -
+                                           points[i][0] * points[k][1]))
+        return result

904d6685a53ab500c97e900d7c11ba76ba873e0b
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-08 12:49:00 +0530

    Time: 170 ms (46.9%), Space: 35.2 MB (20.35%) - LeetHub

---
+++ 3584-maximum-product-of-first-and-last-elements-of-a-subsequence/3584-maximum-product-of-first-and-last-elements-of-a-subsequence.py
@@ -0,0 +1,15 @@
+# two pointers, sliding window
+class Solution:
+    def maximumProduct(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        result = mx = float("-inf")
+        mn = float("inf")
+        for i in range(len(nums)-(m-1)):
+            mx = max(mx, nums[i])
+            mn = min(mn, nums[i])
+            result = max(result, nums[i+(m-1)]*mx, nums[i+(m-1)]*mn)
+        return result

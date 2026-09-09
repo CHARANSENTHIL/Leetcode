616a8d35674ca438526d65ab7ef367e0a2c64363
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:45:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (13.17%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -0,0 +1,13 @@
+# hash table
+class Solution:
+    def repeatedCharacter(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        lookup = set()
+        for c in s:
+            if c in lookup:
+                break
+            lookup.add(c)
+        return c

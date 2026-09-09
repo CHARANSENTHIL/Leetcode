8913bed4b536b8ac9a0119d1ea1ac8cb87bffe5d
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 10:33:13 +0530

    Time: 762 ms (10.64%), Space: 34.8 MB (47.52%) - LeetHub

---
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordCount(self, startWords, targetWords):
+        """
+        :type startWords: List[str]
+        :type targetWords: List[str]
+        :rtype: int
+        """
+        def bitmask(w):
+            return reduce(lambda x, y: x|y, (1 << (ord(c)-ord('a')) for i, c in enumerate(w)))
+
+        lookup = set(bitmask(w) for w in startWords)
+        result = 0 
+        for w in targetWords: 
+            mask = bitmask(w)
+            result += any(mask ^ (1 << ord(c)-ord('a')) in lookup for c in w)
+        return result

178ead415bdc15a1fa1cbcbad18df74699e06739
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:43:06 +0530

    Time: 6 ms (6.29%), Space: 19.3 MB (69.25%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -0,0 +1,20 @@
+import itertools
+
+
+# string, hash table
+class Solution:
+    def decodeMessage(self, key, message):
+        """
+        :type key: str
+        :type message: str
+        :rtype: str
+        """
+        f = lambda x: ord(x)-ord('a')
+        lookup = [-1]*26
+        i = 0
+        for x in map(f, key):
+            if x < 0 or lookup[x] != -1:
+                continue
+            lookup[x] = i
+            i += 1
+        return "".join(map(lambda x: chr(ord('a')+x), (lookup[x] if x >= 0 else x for x in map(f, message))))

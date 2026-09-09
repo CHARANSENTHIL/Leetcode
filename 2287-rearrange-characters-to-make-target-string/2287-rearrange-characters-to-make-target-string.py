e1067bfb0519dc0a226eb8fade3b3e6868db826c
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:42:03 +0530

    Time: 3 ms (13.82%), Space: 19.4 MB (8.82%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -0,0 +1,14 @@
+import collections
+
+
+# freq table
+class Solution:
+    def rearrangeCharacters(self, s, target):
+        """
+        :type s: str
+        :type target: str
+        :rtype: int
+        """
+        cnt1 = collections.Counter(s)
+        cnt2 = collections.Counter(target)
+        return min(cnt1[k]//v for k, v in cnt2.items())

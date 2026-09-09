9485341d6860639fe39e480730708962d34a5d09
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:41:41 +0530

    Time: 3 ms (17.41%), Space: 19.2 MB (61.21%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -0,0 +1,12 @@
+import collections
+
+
+# freq table
+class Solution:
+    def digitCount(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        cnt = collections.Counter(num)
+        return all(cnt[str(i)] == int(x) for i, x in enumerate(num))

53cdf8ed835a960697d0d65e73debf2498fcbf47
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:33:15 +0530

    Time: 5 ms (32.13%), Space: 19.7 MB (28.96%) - LeetHub

--- 0893-groups-of-special-equivalent-strings/0893-groups-of-special-equivalent-strings.py
+++ 0893-groups-of-special-equivalent-strings/0893-groups-of-special-equivalent-strings.py
@@ -1,14 +1,13 @@
 class Solution:
-    def numSpecialEquivGroups(self, words: List[str]) -> int:
-                                                            #  Example: 
-        wSet = set()                                        #   words = ["abc","acb","bac","bca","cab","cba"]
+    def numSpecialEquivGroups(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        def count(word):
+            result = [0]*52
+            for i, letter in enumerate(word):
+                result[ord(letter)-ord('a') + 26*(i%2)] += 1
+            return tuple(result)
 
-        for word in words:                                  #           sorted((enu-   
-            word = tuple(sorted((enumerate(word)),          #  word     merate word)         wSet
-                           key = lambda x: (x[0]%2,x[1])))  #  –––––    ––––––––––––         ––––––––––––––
-                                                            #   abc     ((0,a),(2,c),(1,b))  {(a,c,b)}
-            wSet.add(list(zip(*word))[1])                   #   acb     ((0,a),(2,b),(1,c))  {(a,c,b), (a,b,c)}
-                                                            #   bac     ((0,b),(2,c),(1,a))  {(a,c,b), (a,b,c), (b,c,a)}
-        return len(wSet)                                    #   bca     ((2,a),(0,b),(1,c))  {(a,c,b), (a,b,c), (b,c,a)}
-                                                            #   cab     ((2,b),(0,c),(1,a))  {(a,c,b), (a,b,c), (b,c,a)}
-                                                            #   cba     ((2,a),(0,c),(1,b))  {(a,c,b), (a,b,c), (b,c,a)}
+        return len({count(word) for word in A})

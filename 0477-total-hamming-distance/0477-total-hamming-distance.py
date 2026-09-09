ea5d2ee208b895f63ec1d78a4efaa9622ef40868
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 09:46:27 +0530

    Time: 186 ms (26.58%), Space: 20.7 MB (35.19%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalHammingDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for i in range(32):
+            counts = [0] * 2
+            for num in nums:
+                counts[(num >> i) & 1] += 1
+            result += counts[0] * counts[1]
+        return result

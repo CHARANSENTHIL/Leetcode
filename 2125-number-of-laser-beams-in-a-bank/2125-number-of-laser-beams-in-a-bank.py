8acc609eea8413102d7787e643ed5dcce2e593be
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 10:32:32 +0530

    Time: 3 ms (67.32%), Space: 21.3 MB (29.72%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfBeams(self, bank):
+        """
+        :type bank: List[str]
+        :rtype: int
+        """
+        result = prev = 0
+        for x in bank:
+            cnt = x.count('1')
+            if not cnt:
+                continue
+            result += prev*cnt
+            prev = cnt
+        return result

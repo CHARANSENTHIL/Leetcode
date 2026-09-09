5b6a967c60d17d85ae8475dd06b8144b6129d0a9
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:42:41 +0530

    Time: 104 ms (94.55%), Space: 30.6 MB (25.45%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -0,0 +1,16 @@
+# hash table, math
+class Solution:
+    def distinctNames(self, ideas):
+        """
+        :type ideas: List[str]
+        :rtype: int
+        """
+        lookup = [set() for _ in range(26)]
+        for x in ideas:
+            lookup[ord(x[0])-ord('a')].add(x[1:])
+        result = 0
+        for i in range(len(lookup)):
+            for j in range(i+1, len(lookup)):
+                common = len(lookup[i]&lookup[j])
+                result += (len(lookup[i])-common)*(len(lookup[j])-common)
+        return result*2

f2dd374de284b701f59c2f8186f832969a6c3cc6
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-09 11:44:49 +0530

    Time: 79 ms (6.06%), Space: 30.5 MB (65.15%) - LeetHub

---
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
@@ -0,0 +1,17 @@
+# constructive algorithms
+class Solution:
+    def shortestSequence(self, rolls, k):
+        """
+        :type rolls: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l = 0
+        lookup = set()
+        for x in rolls:
+            lookup.add(x)
+            if len(lookup) != k:
+                continue
+            lookup.clear()
+            l += 1
+        return l+1

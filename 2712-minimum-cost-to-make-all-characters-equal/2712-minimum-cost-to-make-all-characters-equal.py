04620634dbd540718a610c3cc981af252419ca60
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:27:04 +0530

    Time: 86 ms (23.15%), Space: 19.9 MB (49.07%) - LeetHub

---
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.py
@@ -0,0 +1,8 @@
+# greedy
+class Solution:
+    def minimumCost(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return sum(min(i+1, len(s)-(i+1)) for i in range(len(s)-1) if s[i] != s[i+1])

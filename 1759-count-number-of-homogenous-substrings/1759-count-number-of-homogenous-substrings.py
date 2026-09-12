488dc65e96181419d874a215f036d06b0a41ec9d
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:58:41 +0530

    Time: 91 ms (28.72%), Space: 19.8 MB (67.13%) - LeetHub

---
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countHomogenous(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        MOD = 10**9+7
+        result = cnt = 0
+        for i in range(len(s)):
+            if i and s[i-1] == s[i]:
+                cnt += 1
+            else:
+                cnt = 1
+            result = (result+cnt)%MOD
+        return result

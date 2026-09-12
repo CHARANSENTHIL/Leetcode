4568c8bac75798b85a2ab51d85371293d66a31d0
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:05:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (21.54%) - LeetHub

---
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.py
@@ -0,0 +1,9 @@
+# combinatorics, fast exponentiation
+class Solution:
+    def monkeyMove(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        return (pow(2, n, MOD)-2)%MOD

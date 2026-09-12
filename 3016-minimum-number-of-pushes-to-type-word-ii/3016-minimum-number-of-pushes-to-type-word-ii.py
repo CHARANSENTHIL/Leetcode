b9870ed4d9981a7df08ddc8acb808c2f18a2f05d
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:16:24 +0530

    Time: 100 ms (68.36%), Space: 20.1 MB (17.67%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,11 @@
+import collections
+
+
+# freq table, greedy
+class Solution:
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        return sum(x*(i//(9-2+1)+1) for i, x in enumerate(sorted(collections.Counter(word).values(), reverse=True)))

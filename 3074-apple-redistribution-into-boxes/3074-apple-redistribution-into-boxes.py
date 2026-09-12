212c0825f9e25aa7f57cd66a4cd9209a58561df3
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:18:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.29%) - LeetHub

---
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.py
@@ -0,0 +1,15 @@
+# sort, greedy
+class Solution:
+    def minimumBoxes(self, apple, capacity):
+        """
+        :type apple: List[int]
+        :type capacity: List[int]
+        :rtype: int
+        """
+        capacity.sort(reverse=True)
+        total = sum(apple)
+        for i in range(len(capacity)):
+            total -= capacity[i]
+            if total <= 0:
+                return i+1
+        return -1

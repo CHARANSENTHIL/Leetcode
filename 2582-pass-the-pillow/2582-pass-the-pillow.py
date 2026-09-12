27395b72b05b256dfb340d41dd56f94056129cfa
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:06:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.87%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -0,0 +1,9 @@
+# math
+class Solution:
+    def passThePillow(self, n, time):
+        """
+        :type n: int
+        :type time: int
+        :rtype: int
+        """
+        return n-abs((n-1)-(time%(2*(n-1))))

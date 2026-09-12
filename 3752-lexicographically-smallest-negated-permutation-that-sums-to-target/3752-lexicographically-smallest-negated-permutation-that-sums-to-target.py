f5b4823ad94ca0a643004a64698f6716c9cc9bdb
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:36:19 +0530

    Time: 287 ms (23.53%), Space: 28.9 MB (96.64%) - LeetHub

---
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.py
@@ -0,0 +1,26 @@
+# greedy, two pointers
+class Solution:
+    def lexSmallestNegatedPerm(self, n, target):
+        """
+        :type n: int
+        :type target: int
+        :rtype: List[int]
+        """
+        def count(x):
+            return (x+1)*x//2
+
+        total = count(n)
+        if abs(target) > total or (target-total)%2:
+            return []
+        result = [0]*n
+        left, right = 0, n-1
+        for i in reversed(range(1, n+1)):
+            if target-(-i) <= count(i-1):
+                target -= -i
+                result[left] = -i
+                left += 1
+            else:
+                target -= i
+                result[right] = i
+                right -= 1
+        return result

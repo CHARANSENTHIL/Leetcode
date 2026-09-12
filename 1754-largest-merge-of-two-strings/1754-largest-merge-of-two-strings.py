0f444473887ac2fb78e271084a2bc3d1fa12e284
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:58:15 +0530

    Time: 184 ms (16%), Space: 19.4 MB (78.22%) - LeetHub

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -0,0 +1,19 @@
+import collections
+
+
+class Solution:
+    def largestMerge(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        q1 = collections.deque(word1)
+        q2 = collections.deque(word2)
+        result = []
+        while q1 or q2:
+            if q1 > q2:
+                result.append(q1.popleft())
+            else:
+                result.append(q2.popleft())
+        return "".join(result)

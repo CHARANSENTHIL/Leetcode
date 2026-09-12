63a4113a6a082b19d16240c6343183d8c071d15d
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:26:23 +0530

    Time: 69 ms (14.88%), Space: 19.4 MB (52.99%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.py
@@ -0,0 +1,8 @@
+# greedy
+class Solution:
+    def makeSmallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return "".join(min(s[i], s[~i]) for i in range(len(s)))

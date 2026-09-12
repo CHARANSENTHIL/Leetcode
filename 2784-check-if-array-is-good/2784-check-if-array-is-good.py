e4783c24c68b315f7d363aca00276c26e7b779ee
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:09:14 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.08%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -0,0 +1,14 @@
+# freq table
+class Solution:
+    def isGood(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cnt = [0]*len(nums)
+        for x in nums:
+            if x < len(cnt):
+                cnt[x] += 1
+            else:
+                return False
+        return all(cnt[x] == 1 for x in range(1, len(nums)-1))

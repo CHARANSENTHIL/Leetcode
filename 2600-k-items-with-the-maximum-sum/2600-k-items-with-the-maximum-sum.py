0ed2d8fb7715c021026438c1aa61b1af77183cae
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 09:08:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.52%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
@@ -0,0 +1,11 @@
+# greedy, math
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes, numZeros, numNegOnes, k):
+        """
+        :type numOnes: int
+        :type numZeros: int
+        :type numNegOnes: int
+        :type k: int
+        :rtype: int
+        """
+        return min(numOnes, k)-max(k-numOnes-numZeros, 0)

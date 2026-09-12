f320ca352cc1273806d5719138141575d304effb
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:17:45 +0530

    Time: 30 ms (57.14%), Space: 27.9 MB (88.24%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -0,0 +1,17 @@
+# greedy
+class Solution:
+    def maximumValueSum(self, nums, k, edges):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        result = parity = 0
+        diff = float("inf")
+        for x in nums:
+            y = x^k
+            result += max(x, y)
+            parity ^= int(x < y)
+            diff = min(diff, abs(x-y))
+        return result-parity*diff

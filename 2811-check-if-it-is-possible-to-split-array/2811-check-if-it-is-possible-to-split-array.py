2a02591562bef0dc6c92f0d0043970b4b1b38e49
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:27:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (75%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.py
@@ -0,0 +1,9 @@
+# constructive algorithms
+class Solution:
+    def canSplitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: bool
+        """
+        return len(nums) <= 2 or any(nums[i]+nums[i+1] >= m for i in range(len(nums)-1))

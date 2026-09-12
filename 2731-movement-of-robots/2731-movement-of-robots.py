389026bd8f0645c1d071daec9a18c262c9b12507
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-12 11:06:58 +0530

    Time: 102 ms (13.77%), Space: 30.4 MB (82.61%) - LeetHub

---
+++ 2731-movement-of-robots/2731-movement-of-robots.py
@@ -0,0 +1,14 @@
+# sort, math
+class Solution:
+    def sumDistance(self, nums, s, d):
+        """
+        :type nums: List[int]
+        :type s: str
+        :type d: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        for i in range(len(nums)):
+            nums[i] += d if s[i] == 'R' else -d
+        nums.sort()
+        return reduce(lambda x, y: (x+y)%MOD, ((i-(len(nums)-(i+1)))*x for i, x in enumerate(nums)))

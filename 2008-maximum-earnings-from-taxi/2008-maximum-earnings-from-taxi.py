d98f7082d83e282c05c3dca9fc17f928b5116071
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-23 09:08:48 +0530

    Time: 295 ms (61.68%), Space: 37 MB (98.19%) - LeetHub

---
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxTaxiEarnings(self, n, rides):
+        """
+        :type n: int
+        :type rides: List[List[int]]
+        :rtype: int
+        """
+        rides.sort()
+        dp = [0]*(n+1)
+        j = 0
+        for i in range(1, n+1):
+            dp[i] = max(dp[i], dp[i-1])
+            while j < len(rides) and rides[j][0] == i:
+                dp[rides[j][1]] = max(dp[rides[j][1]], dp[i]+rides[j][1]-rides[j][0]+rides[j][2])
+                j += 1
+        return dp[-1]

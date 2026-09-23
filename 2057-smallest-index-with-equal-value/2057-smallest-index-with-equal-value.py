260576200dde9cbe086413cfb30009703333dde8
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-23 09:16:29 +0530

    Time: 3 ms (11.2%), Space: 19.2 MB (85.34%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
@@ -0,0 +1,7 @@
+class Solution:
+    def smallestEqual(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return next((i for i, x in enumerate(nums) if i%10 == x), -1)

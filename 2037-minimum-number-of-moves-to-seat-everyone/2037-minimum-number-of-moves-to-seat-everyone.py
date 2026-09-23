d70523b0484702747cd77ec6963e46de04b7c51a
CHARANSENTHIL <charanharshini7@gmail.com>
2026-09-23 09:13:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (56.37%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,13 @@
+import itertools
+
+
+class Solution:
+    def minMovesToSeat(self, seats, students):
+        """
+        :type seats: List[int]
+        :type students: List[int]
+        :rtype: int
+        """
+        seats.sort()
+        students.sort()
+        return sum(abs(a-b) for a, b in zip(seats, students))
